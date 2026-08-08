6d83ad99eeb16e904b119e5cda5500516be23c3a
ogbesuruyona-hub <ogbesuruyona@icloud.com>
2026-08-08 18:07:53 -0400

    Enable GA4 visitor analytics
    Install the GA4 tag across public pages and allow the official Google tag script through CSP.

---
+++ analytics.js
@@ -0,0 +1,19 @@
+(() => {
+  const measurementId = "G-9PGWZG1D9Y";
+
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = window.gtag || function gtag() {
+    window.dataLayer.push(arguments);
+  };
+
+  window.gtag("js", new Date());
+  window.gtag("config", measurementId, {
+    anonymize_ip: true,
+    transport_type: "beacon",
+  });
+
+  const googleTag = document.createElement("script");
+  googleTag.async = true;
+  googleTag.src = `https://www.googletagmanager.com/gtag/js?id=${encodeURIComponent(measurementId)}`;
+  document.head.append(googleTag);
+})();
